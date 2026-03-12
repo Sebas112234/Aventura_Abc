b0d7567f7fac3492618d5e9d149bc51a76dd9be9
MonicaE128 <a22300942@ceti.mx>
2026-03-12 12:24:53 -0600

    Avance4ContarObjetos

--- Assets/Scripts/ContarObjetos/ContadorObjetos.cs
+++ Assets/Scripts/ContarObjetos/ContadorObjetos.cs
@@ -8,6 +8,7 @@ public class ContadorObjetos : MonoBehaviour
     public Transform Espacio2;
     public Transform Espacio3;
 
+
     int cantidad1;
     int cantidad2;
     int cantidad3;
@@ -21,9 +22,15 @@ public class ContadorObjetos : MonoBehaviour
 
     void GenerarNivel()
     {
-        cantidad1 = Random.Range(1, 10);
-        cantidad2 = Random.Range(1, 10);
-        cantidad3 = Random.Range(1, 10);
+        // Generar tres cantidades distintas entre 1 y 9
+        int min = 1;
+        int max = 10; // Random.Range int es inclusive min, exclusive max
+
+        cantidad1 = Random.Range(min, max);
+        // asegurar que cantidad2 sea diferente a cantidad1
+        do { cantidad2 = Random.Range(min, max); } while (cantidad2 == cantidad1);
+        // asegurar que cantidad3 sea diferente a cantidad1 y cantidad2
+        do { cantidad3 = Random.Range(min, max); } while (cantidad3 == cantidad1 || cantidad3 == cantidad2);
 
         GenerarManzanas(Espacio1, cantidad1);
         GenerarManzanas(Espacio2, cantidad2);
@@ -45,7 +52,22 @@ public class ContadorObjetos : MonoBehaviour
                 Random.Range(-100, 100)
             );
 
-            Instantiate(ObjetoPrefab, Espacio).GetComponent<RectTransform>().anchoredPosition = posicion;
+            // Instanciar como hijo sin mantener la misma posición y ajustar transformaciones
+            GameObject go = Instantiate(ObjetoPrefab, Espacio, false);
+            RectTransform rt = go.GetComponent<RectTransform>();
+            if (rt != null)
+            {
+                rt.anchoredPosition = posicion;
+                rt.localScale = ObjetoPrefab.transform.localScale;
+            }
+            else
+            {
+                go.transform.localPosition = new Vector3(posicion.x, posicion.y, 0f);
+                go.transform.localScale = ObjetoPrefab.transform.localScale;
+            }
+
+            // Dar nombre único para facilitar la depuración
+            go.name = ObjetoPrefab.name + "_" + i;
         }
     }
 
