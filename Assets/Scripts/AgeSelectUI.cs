dcb38751ef302478d1fc895811fa706441b065ac
a22300950 <a22300950@ceti.mx>
2026-04-30 12:07:45 -0600

    Selector_Edad
    Guardar la edad en el historial

--- Assets/Scripts/AgeSelectUI.cs
+++ Assets/Scripts/AgeSelectUI.cs
@@ -6,12 +6,14 @@ public class AgeSelectUI : MonoBehaviour
     public void Select3to5()
     {
         PlayerPrefs.SetInt("AgeGroup", 1);
-        SceneManager.LoadScene("01_Welcome"); 
+        PlayerPrefs.Save(); // Forzamos el guardado en disco
+        SceneManager.LoadScene("Levels_2_4"); // Cambié a la escena de niveles correspondiente
     }
 
     public void Select6to8()
     {
         PlayerPrefs.SetInt("AgeGroup", 2);
+        PlayerPrefs.Save(); 
         SceneManager.LoadScene("04_Levels_5_7"); 
     }
 
