b05fc1267dc65384a533d0c56c496a0eefecf243
Sebas112234 <sebasiggo@gmail.com>
2026-04-30 10:18:04 -0600

    Despla_2

--- Assets/Scripts/Levels_5_7_UI.cs
+++ Assets/Scripts/Levels_5_7_UI.cs
@@ -16,4 +16,37 @@ public class Levels_5_7_UI : MonoBehaviour
     {
         SceneManager.LoadScene("SimonDice");
     }
+    public void ContarObjetos()
+    {
+        SceneManager.LoadScene("ContarObjetos");
+    }
+    public void TrazadoLetras()
+    {
+        SceneManager.LoadScene("TrazadoLetras");
+    }
+    public void TrazadoFiguras()
+    {
+        SceneManager.LoadScene("TrazarFiguras");
+    }
+    public void EscucharPalabras()
+    {
+        SceneManager.LoadScene("07_ListenWords");
+    }
+    public void FormarPalabras()
+    {
+        SceneManager.LoadScene("FormarPalabras");
+    }
+    public void SumasRestas()
+    {
+        SceneManager.LoadScene("06_Math_5_7");
+    }
+    public void EncontrarPalabras()
+    {
+        SceneManager.LoadScene("EncontrarPalabras");
+    }
+    public void Memorama()
+    {
+        SceneManager.LoadScene("Memorama");
+    }
+
 }
